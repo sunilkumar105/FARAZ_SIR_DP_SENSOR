248e8b4b19271a712df6e2769fe425f0c97fdacf
sunilkumar105 <sunilkumar62394@gmail.com>
2023-12-17 12:18:57 +0530

    dp will only reply to command added

--- DP_F103C8T6/Core/Src/main.c
+++ DP_F103C8T6/Core/Src/main.c
@@ -23,6 +23,7 @@
 /* USER CODE BEGIN Includes */
 #include "nrf_helper.h"
 #include "utilities.h"
+#include "ADP8XX_DP_SENSOR.h"
 #include "ee.h"
 /* USER CODE END Includes */
 
@@ -42,8 +43,6 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc1;
-
 I2C_HandleTypeDef hi2c1;
 
 SPI_HandleTypeDef hspi1;
@@ -61,7 +60,6 @@ static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_ADC1_Init(void);
 static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
@@ -104,20 +102,18 @@ int main(void) {
 	MX_SPI1_Init();
 	MX_USART1_UART_Init();
 	MX_USART2_UART_Init();
-	MX_ADC1_Init();
 	MX_I2C1_Init();
 	/* USER CODE BEGIN 2 */
 
-	HAL_UART_Receive_IT(&huart2, (uint8_t*) rx_data, 1); //INITIALIZE THE INTERRUPT
-
-	uint8_t datawrite[4] = { 'A', 'B', 'C', 'D' };
-	ee_write(_EE_ADDR_INUSE, datawrite, 4);
-	uint8_t dataread[4];
-	ee_read(_EE_ADDR_INUSE, dataread, 4);
-	HAL_Delay(1000);
-	ee_format_sector();
-
+//	HAL_UART_Receive_IT(&huart2, (uint8_t*) rx_data, 1); //INITIALIZE THE INTERRUPT
+//	uint8_t datawrite[4] = { 'A', 'B', 'C', 'D' };
+//	ee_write(_EE_ADDR_INUSE, datawrite, 4);
+//	uint8_t dataread[4];
+//	ee_read(_EE_ADDR_INUSE, dataread, 4);
+//	HAL_Delay(1000);
+//	ee_format_sector();
 	nrf_init();
+//	Switch_to_Receiver_mode();
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
@@ -126,10 +122,12 @@ int main(void) {
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
-		send_DP_sensor_value_to_gateway();
-//		HANDLE_RECEIVED_MESSAGE_FROM_GATEWAY();
+//		send_DP_sensor_value_to_gateway();
+		if (NRF24_available()) {
+			HANDLE_RECEIVED_MESSAGE_FROM_GATEWAY();
+		}
 		HAL_GPIO_TogglePin(LED_PIN_GPIO_Port, LED_PIN_Pin);
-		HAL_Delay(3000);
+		HAL_Delay(1000);
 	}
 	/* USER CODE END 3 */
 }
@@ -141,7 +139,6 @@ int main(void) {
 void SystemClock_Config(void) {
 	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
 	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
-	RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
 
 	/** Initializes the RCC Oscillators according to the specified parameters
 	 * in the RCC_OscInitTypeDef structure.
@@ -166,55 +163,6 @@ void SystemClock_Config(void) {
 	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
 		Error_Handler();
 	}
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
-	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
-		Error_Handler();
-	}
-}
-
-/**
- * @brief ADC1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_ADC1_Init(void) {
-
-	/* USER CODE BEGIN ADC1_Init 0 */
-
-	/* USER CODE END ADC1_Init 0 */
-
-	ADC_ChannelConfTypeDef sConfig = { 0 };
-
-	/* USER CODE BEGIN ADC1_Init 1 */
-
-	/* USER CODE END ADC1_Init 1 */
-
-	/** Common config
-	 */
-	hadc1.Instance = ADC1;
-	hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-	hadc1.Init.ContinuousConvMode = ENABLE;
-	hadc1.Init.DiscontinuousConvMode = DISABLE;
-	hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-	hadc1.Init.NbrOfConversion = 1;
-	if (HAL_ADC_Init(&hadc1) != HAL_OK) {
-		Error_Handler();
-	}
-
-	/** Configure Regular Channel
-	 */
-	sConfig.Channel = ADC_CHANNEL_0;
-	sConfig.Rank = ADC_REGULAR_RANK_1;
-	sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN ADC1_Init 2 */
-
-	/* USER CODE END ADC1_Init 2 */
-
 }
 
 /**
@@ -271,7 +219,7 @@ static void MX_SPI1_Init(void) {
 	hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
 	hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
 	hspi1.Init.NSS = SPI_NSS_SOFT;
-	hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+	hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
 	hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
 	hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
 	hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
@@ -359,7 +307,6 @@ static void MX_GPIO_Init(void) {
 
 	/* GPIO Ports Clock Enable */
 	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOD_CLK_ENABLE();
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 
@@ -367,7 +314,7 @@ static void MX_GPIO_Init(void) {
 	HAL_GPIO_WritePin(LED_PIN_GPIO_Port, LED_PIN_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(CE_PIN_GPIO_Port, CE_PIN_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, CE_PIN_Pin | CSN_PIN_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin : LED_PIN_Pin */
 	GPIO_InitStruct.Pin = LED_PIN_Pin;
@@ -376,17 +323,12 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(LED_PIN_GPIO_Port, &GPIO_InitStruct);
 
-	/*Configure GPIO pin : CE_PIN_Pin */
-	GPIO_InitStruct.Pin = CE_PIN_Pin;
+	/*Configure GPIO pins : CE_PIN_Pin CSN_PIN_Pin */
+	GPIO_InitStruct.Pin = CE_PIN_Pin | CSN_PIN_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(CE_PIN_GPIO_Port, &GPIO_InitStruct);
-
-	/*Configure GPIO pin : CSN_PIN_Pin */
-	GPIO_InitStruct.Pin = CSN_PIN_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-	HAL_GPIO_Init(CSN_PIN_GPIO_Port, &GPIO_InitStruct);
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 	/* USER CODE BEGIN MX_GPIO_Init_2 */
 	/* USER CODE END MX_GPIO_Init_2 */
--- DP_F103C8T6/Core/Src/utilities.c
+++ DP_F103C8T6/Core/Src/utilities.c
@@ -6,21 +6,45 @@
  */
 
 #include "utilities.h"
-extern ADC_HandleTypeDef hadc1;
+extern UART_HandleTypeDef huart2;
+char my_node_id[2] = "01";
 
 void send_DP_sensor_value_to_gateway(void) {
-	HAL_ADC_Start(&hadc1); //TAKE SAMPLE
-	HAL_ADC_PollForConversion(&hadc1, 100);
-	uint32_t adc_val = HAL_ADC_GetValue(&hadc1);
-	HAL_ADC_Stop(&hadc1);
 
-	char adc_str[32]; //UINT32_T TO CHAR ARRAY CONVERSION
-	sprintf(adc_str, "%lu", adc_val);
-	adc_str[6] = '\r';
-	memcpy(adc_str,"abcdefghijklmnopq      d",32);
-	TRANSMITT_UPDATE_TO_GATEWAY(adc_str); //TRANSMITT MESSAGE TO GATEWAY
+	uint32_t PRESSURE = READ_PRESSURE();
+	char buffer[20];
+	sprintf(buffer, "%u", PRESSURE);
+
+	char my_tx_data[32] = "010";
+	my_tx_data[3] = buffer[0];
+	my_tx_data[4] = buffer[1];
+	my_tx_data[5] = buffer[2];
+	my_tx_data[5] = '\r';
+	my_tx_data[5] = '\n';
+	TRANSMITT_UPDATE_TO_GATEWAY(my_tx_data); //TRANSMITT MESSAGE TO GATEWAY
 }
-void HANDLE_RECEIVED_MESSAGE_FROM_GATEWAY(void){
-	//check for availability of message
-	//take action according to the message
+void HANDLE_RECEIVED_MESSAGE_FROM_GATEWAY(void) {
+	char received_data_from_gateway[50] = "";
+	HAL_UART_Transmit(&huart2, (uint8_t*) "\n DATA RECEIVED FROM NRF::>> ",
+			strlen("\n DATA RECEIVED FROM NRF::>> "), 10);
+	NRF24_read(received_data_from_gateway, 5); //store received data
+	HAL_UART_Transmit(&huart2, (uint8_t*) received_data_from_gateway, 5, 10);
+	char rcvd_node_id[2];
+	memcpy(rcvd_node_id, received_data_from_gateway, 2);
+	char rcvd_command = received_data_from_gateway[2];
+
+	//============CHECK IF NODE MATCH WITH ID======================//
+	if (!(memcmp(rcvd_node_id, my_node_id, 2))) {
+		HAL_UART_Transmit(&huart2, (uint8_t*) "\n NODE ID MATCHED",
+				strlen("\n NODE ID MATCHED"), 10);
+	} else {
+		return;
+	}
+	//Read the command and transmitt the DP SENSOR VAL
+	if (rcvd_command == '0') {
+		//transmitt a valuye
+		HAL_UART_Transmit(&huart2, (uint8_t*) "\n TRANSMITTING DP DATA",
+				strlen("\n TRANSMITTING DP DATA"), 10);
+		send_DP_sensor_value_to_gateway();
+	}
 }
